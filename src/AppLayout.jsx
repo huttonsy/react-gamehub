edfaea154ce310e51ef4ac34be8610d246b19d83
Abbyswalk <aswalker205@gmail.com>
2025-04-17 11:25:06 -0400

    styling nav bar

--- src/AppLayout.jsx
+++ src/AppLayout.jsx
@@ -1,20 +1,20 @@
 import { Outlet, NavLink } from "react-router-dom";
+import "./AppLayout.css";
 
 export default function AppLayout() {
   return (
-    <div style={{ display: "flex", flexDirection: "column", minHeight: "100vh" }}>
-      <header>
-        <nav>
+    <div className="layout">
+      <header className="navbar">
+        <nav className="nav-links">
           <NavLink to="/">Home</NavLink> |  
           <NavLink to="/game/tic-tac-toe"> Tic Tac Toe </NavLink> |
           <NavLink to="/wordle"> Wordle </NavLink> |
           <NavLink to="/rps"> Rock Paper Scissors </NavLink> |
           <NavLink to="/memory"> Memory Game </NavLink> |
           <NavLink to="/hangman"> Hangman </NavLink>
-
         </nav>
       </header>
-      <main style={{ flexGrow: 1, overflowY: "auto" }}>
+      <main className="main-content">
         <Outlet />
       </main>
     </div>
